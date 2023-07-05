f3bf622a8cb608cf0d135b2e33e7f77298e945b6
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-05 21:37:08 +0200

    Log lifecycle hooks in login activity

--- app/src/main/java/infinuma/android/shows/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -2,10 +2,37 @@ package infinuma.android.shows
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 
 class LoginActivity:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Login activity", "onCreate")
         setContentView(R.layout.login_activity_layout)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Login activity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Login activity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Login activity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Login activity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Login activity", "onDestroy")
     }
 }

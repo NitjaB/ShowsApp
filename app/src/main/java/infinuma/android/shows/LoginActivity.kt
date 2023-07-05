7e09a7058625be629e3cbde8d456370372841bc0
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-05 20:25:05 +0200

    Add LoginActivity

---
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -0,0 +1,11 @@
+package infinuma.android.shows
+
+import android.app.Activity
+import android.os.Bundle
+
+class LoginActivity:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity_layout)
+    }
+}

a908d4d473c26d64feb75193be1104bf02e026a4
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-07 23:41:10 +0200

    Apply Infinum code style

--- app/src/main/java/infinuma/android/shows/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 
-class LoginActivity:Activity() {
+class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.i("Login activity", "onCreate")

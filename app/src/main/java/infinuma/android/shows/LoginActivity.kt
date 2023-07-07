2e7f73b1027a1aef9382c93bd94c288e9bd8b419
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-07 21:25:26 +0200

    Add lifecycle logs from login activity as a comment

--- app/src/main/java/infinuma/android/shows/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -35,4 +35,45 @@ class LoginActivity:Activity() {
         super.onDestroy()
         Log.i("Login activity", "onDestroy")
     }
+
+    /*
+      Put the app in background and move it back to foreground
+      onCreate()
+      onStart()
+      onResume() - then move to background
+      onPause()
+      onStop() - then move to foreground
+      onStart()
+      onResume()
+    */
+
+    /*
+      Kill the app when it's in background
+      onCreate()
+      onStart()
+      onResume() - then move to background
+      onPause()
+      onStop() - then kill app
+      onDestroy()
+
+
+      Kill the app when it's in foreground
+      onCreate()
+      onStart()
+      onResume() - then kill app with call finish()
+      onPause()
+      onStop()
+      onDestroy()
+    */
+
+    /*
+      Lock the phones screen and unlock it
+      onCreate()
+      onStart()
+      onResume() - then lock the phones screen
+      onPause()
+      onStop() - then unlock phones screen
+      onStart()
+      onResume()
+    */
 }

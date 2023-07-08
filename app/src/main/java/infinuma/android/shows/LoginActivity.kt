46bef9b4f3e9fc7007c23f2a8bee8ad1741c97b2
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-08 16:26:29 +0200

    Remove lifecycle logging and comments

--- app/src/main/java/infinuma/android/shows/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -2,78 +2,11 @@ package infinuma.android.shows
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("Login activity", "onCreate")
         setContentView(R.layout.login_activity_layout)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i("Login activity", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("Login activity", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("Login activity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("Login activity", "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Login activity", "onDestroy")
-    }
-
-    /*
-      Put the app in background and move it back to foreground
-      onCreate()
-      onStart()
-      onResume() - then move to background
-      onPause()
-      onStop() - then move to foreground
-      onStart()
-      onResume()
-    */
-
-    /*
-      Kill the app when it's in background
-      onCreate()
-      onStart()
-      onResume() - then move to background
-      onPause()
-      onStop() - then kill app
-      onDestroy()
-
-
-      Kill the app when it's in foreground
-      onCreate()
-      onStart()
-      onResume() - then kill app with call finish()
-      onPause()
-      onStop()
-      onDestroy()
-    */
-
-    /*
-      Lock the phones screen and unlock it
-      onCreate()
-      onStart()
-      onResume() - then lock the phones screen
-      onPause()
-      onStop() - then unlock phones screen
-      onStart()
-      onResume()
-    */
 }

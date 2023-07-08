eb06cf7c95892b2f31ce1a1e10e4d6fda6834f7d
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-08 23:22:08 +0200

    Make status bar transperent on login scren

--- app/src/main/java/infinuma/android/shows/LoginActivity.kt
+++ app/src/main/java/infinuma/android/shows/LoginActivity.kt
@@ -1,12 +1,20 @@
 package infinuma.android.shows
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        makeStatusBarTransparent()
         setContentView(R.layout.login_activity_layout)
     }
 
+    private fun makeStatusBarTransparent() {
+        window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window?.statusBarColor = Color.TRANSPARENT
+    }
 }

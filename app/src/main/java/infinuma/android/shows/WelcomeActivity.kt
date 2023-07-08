485188789635b19938e3bc766757772e05ebc053
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-09 00:15:33 +0200

    Add welcome screen

---
+++ app/src/main/java/infinuma/android/shows/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package infinuma.android.shows
+
+import android.app.Activity
+import android.os.Bundle
+
+class WelcomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_activity_layout)
+    }
+}

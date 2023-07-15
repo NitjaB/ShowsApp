18b33ba125e977f26dac3615d8b49b0678ece51d
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-15 18:39:45 +0200

    Add show repository

---
+++ app/src/main/java/infinuma/android/shows/shows/data/ShowsRepository.kt
@@ -0,0 +1,64 @@
+package infinuma.android.shows.shows.data
+
+import infinuma.android.shows.R
+import infinuma.android.shows.shows.models.ShowsUi
+
+class ShowsRepository {
+
+    fun getShows() = listOf(
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_office, R.string.the_office_title
+        ),
+        ShowsUi(
+            1, R.drawable.ic_krv_nije_voda, R.string.krv_nije_voda_title,
+        ),
+        ShowsUi(
+            1, R.drawable.ic_stranger_things, R.string.stranger_things_title
+        ),
+    )
+}

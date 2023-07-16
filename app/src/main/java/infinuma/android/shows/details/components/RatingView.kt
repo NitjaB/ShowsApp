ef4b68990ce9dd3c47f5fe262c4cf351001e0f6f
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-16 17:20:03 +0200

    Show reviews in review view

--- app/src/main/java/infinuma/android/shows/details/components/RatingView.kt
+++ app/src/main/java/infinuma/android/shows/details/components/RatingView.kt
@@ -5,8 +5,10 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
 import infinuma.android.shows.R
+import infinuma.android.shows.databinding.ReviewItemBinding
 import infinuma.android.shows.databinding.ReviewLayoutBinding
 import infinuma.android.shows.details.models.RatingUi
+import infinuma.android.shows.details.models.ReviewUi
 
 class RatingView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
 
@@ -16,5 +18,18 @@ class RatingView(context: Context, attrs: AttributeSet) : FrameLayout(context, a
         binding.info.text =
             context.getString(R.string.show_details_screen_reviews_info, ratingUi.numberOfReviews, ratingUi.averageReviewGrade)
         binding.ratingBar.rating = ratingUi.averageReviewGrade
+        setUpReviews(ratingUi.reviews)
+    }
+
+    private fun setUpReviews(ratings: List<ReviewUi>) {
+        ratings.forEach { item ->
+            val reviewBinding = ReviewItemBinding.inflate(LayoutInflater.from(context)).apply {
+                avatarImageView.setImageResource(item.avatar)
+                usernameTextView.text = item.username
+                ratingTextView.text = item.starGrade.toString()
+                descriptionTextView.text = item.review
+            }
+            binding.reviewsLinearLayout.addView(reviewBinding.root)
+        }
     }
 }

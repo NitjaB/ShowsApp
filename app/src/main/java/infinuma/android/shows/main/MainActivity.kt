39451c39fccd00397b8f35fd526c0880474cb11f
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-18 20:13:13 +0200

    Add layout for main activiy and show it

--- app/src/main/java/infinuma/android/shows/main/MainActivity.kt
+++ app/src/main/java/infinuma/android/shows/main/MainActivity.kt
@@ -2,11 +2,16 @@ package infinuma.android.shows.main
 
 import android.app.Activity
 import android.os.Bundle
+import infinuma.android.shows.databinding.MainActivityLayoutBinding
 import infinuma.android.shows.utils.makeStatusBarTransparent
 
 class MainActivity : Activity() {
+
+    private lateinit var binding: MainActivityLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         makeStatusBarTransparent()
+        binding = MainActivityLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

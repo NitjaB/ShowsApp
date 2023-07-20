85fffea5ff3f20ef25faf003d8d98022d1a759d2
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-20 22:07:38 +0200

    Implement login repository in login screen

--- app/src/main/java/infinuma/android/shows/login/LoginFragment.kt
+++ app/src/main/java/infinuma/android/shows/login/LoginFragment.kt
@@ -1,27 +1,25 @@
 package infinuma.android.shows.login
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CompoundButton
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import infinuma.android.shows.R
 import infinuma.android.shows.databinding.LoginActivityLayoutBinding
 import infinuma.android.shows.login.domain.LoginInputValidator
+import infinuma.android.shows.login.domain.LoginRepository
+import infinuma.android.shows.utils.SharedPrefsSource
 
 class LoginFragment : Fragment() {
-
-    companion object {
-        private const val SHARED_PREFS_REMEMBER_ME = "sharedPrefsRememberMe"
-    }
 
     private lateinit var binding: LoginActivityLayoutBinding
 
     private val inputValidator = LoginInputValidator()
+
+    private val loginRepository = LoginRepository(SharedPrefsSource.getSharedPrefs())
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = LoginActivityLayoutBinding.inflate(layoutInflater)
@@ -30,7 +28,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (getSharedPrefsRememberMe()) {
+        if (loginRepository.isUserRemembered()) {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToShowsFragment())
         } else {
             binding.usernameInputEditText.addTextChangedListener(
@@ -64,16 +62,7 @@ class LoginFragment : Fragment() {
 
     private fun handleLoginButtonClick() {
         val rememberMe = binding.saveLoginCheckBox.isChecked
-        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
-        with(sharedPref.edit()) {
-            putBoolean(SHARED_PREFS_REMEMBER_ME, rememberMe)
-            apply()
-        }
+        loginRepository.setRememberedUser(rememberMe)
         findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToShowsFragment())
-    }
-
-    private fun getSharedPrefsRememberMe(): Boolean {
-        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
-        return sharedPref.getBoolean(SHARED_PREFS_REMEMBER_ME, false)
     }
 }

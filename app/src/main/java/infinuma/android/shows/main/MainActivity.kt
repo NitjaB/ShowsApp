5d0a0b4d2f1f9570a1a8cc0d0e8411c5f4eeaece
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-20 22:05:56 +0200

    Initialize shared prefs singleton

--- app/src/main/java/infinuma/android/shows/main/MainActivity.kt
+++ app/src/main/java/infinuma/android/shows/main/MainActivity.kt
@@ -3,6 +3,7 @@ package infinuma.android.shows.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import infinuma.android.shows.databinding.MainActivityLayoutBinding
+import infinuma.android.shows.utils.SharedPrefsSource
 import infinuma.android.shows.utils.makeStatusBarTransparent
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SharedPrefsSource.init(applicationContext)
         makeStatusBarTransparent()
         binding = MainActivityLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)

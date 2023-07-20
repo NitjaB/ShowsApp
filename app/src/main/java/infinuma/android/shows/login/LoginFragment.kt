d575db980c60d330338a027cc39e5a562acbfd3b
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-20 20:41:57 +0200

    Implement remember me feature on login screen

--- app/src/main/java/infinuma/android/shows/login/LoginFragment.kt
+++ app/src/main/java/infinuma/android/shows/login/LoginFragment.kt
@@ -1,18 +1,23 @@
 package infinuma.android.shows.login
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import androidx.navigation.fragment.findNavController
 import infinuma.android.shows.R
 import infinuma.android.shows.databinding.LoginActivityLayoutBinding
 import infinuma.android.shows.login.domain.LoginInputValidator
 
 class LoginFragment : Fragment() {
+
+    companion object {
+        private const val SHARED_PREFS_REMEMBER_ME = "sharedPrefsRememberMe"
+    }
 
     private lateinit var binding: LoginActivityLayoutBinding
 
@@ -25,14 +30,18 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.usernameInputEditText.addTextChangedListener(
-            afterTextChanged = { email -> handleEmailInputChange(email.toString()) }
-        )
-        binding.passwordInputEditText.addTextChangedListener(
-            afterTextChanged = { password -> handlePasswordInputChange(password.toString()) }
-        )
-        binding.loginButton.setOnClickListener {
+        if (getSharedPrefsRememberMe()) {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToShowsFragment())
+        } else {
+            binding.usernameInputEditText.addTextChangedListener(
+                afterTextChanged = { email -> handleEmailInputChange(email.toString()) }
+            )
+            binding.passwordInputEditText.addTextChangedListener(
+                afterTextChanged = { password -> handlePasswordInputChange(password.toString()) }
+            )
+            binding.loginButton.setOnClickListener {
+                handleLoginButtonClick()
+            }
         }
     }
 
@@ -51,5 +60,20 @@ class LoginFragment : Fragment() {
             binding.usernameInputEditText.text.toString(),
             password,
         )
+    }
+
+    private fun handleLoginButtonClick() {
+        val rememberMe = binding.saveLoginCheckBox.isChecked
+        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putBoolean(SHARED_PREFS_REMEMBER_ME, rememberMe)
+            apply()
+        }
+        findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToShowsFragment())
+    }
+
+    private fun getSharedPrefsRememberMe(): Boolean {
+        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        return sharedPref.getBoolean(SHARED_PREFS_REMEMBER_ME, false)
     }
 }

353fe60089cc9a8d0ba85e7f02e7a533cd5181c6
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-21 21:50:01 +0200

    Add LogoutBottomSheetDialog

---
+++ app/src/main/java/infinuma/android/shows/logout/LogoutBottomSheetDialog.kt
@@ -0,0 +1,33 @@
+package infinuma.android.shows.logout
+
+import android.content.Context
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import infinuma.android.shows.databinding.LogoutDialogBinding
+import infinuma.android.shows.logout.model.LogoutBottomSheetDialogUi
+
+class LogoutBottomSheetDialog(
+    private val state: LogoutBottomSheetDialogUi,
+    private val onLogoutClick: () -> Unit,
+    private val onChangeProfilePictureClick: () -> Unit,
+    context: Context
+) : BottomSheetDialog(context) {
+
+    private lateinit var binding: LogoutDialogBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LogoutDialogBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.usernameTextView.text = state.email
+        binding.profilePictureImageView.setImageResource(state.avatar)
+        binding.logoutButton.setOnClickListener {
+            dismiss()
+            onLogoutClick()
+        }
+        binding.changeProfilePictureButton.setOnClickListener {
+            dismiss()
+            onChangeProfilePictureClick()
+        }
+    }
+}
---
+++ app/src/main/java/infinuma/android/shows/logout/model/LogoutBottomSheetDialogUi.kt
@@ -0,0 +1,8 @@
+package infinuma.android.shows.logout.model
+
+import androidx.annotation.DrawableRes
+
+data class LogoutBottomSheetDialogUi (
+    val email: String,
+    @DrawableRes val  avatar: Int
+)

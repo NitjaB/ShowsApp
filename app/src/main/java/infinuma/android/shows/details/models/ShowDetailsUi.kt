e54e836e9168ff628c7b743ea3b60f37cf7d6a04
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-24 15:01:32 +0200

    Implemnt show details view model
    + add ui model for show details screen

---
+++ app/src/main/java/infinuma/android/shows/details/models/ShowDetailsUi.kt
@@ -0,0 +1,7 @@
+package infinuma.android.shows.details.models
+
+data class ShowDetailsUi(
+    val title: String?= null,
+    val username: String? = null,
+    val ratingUi: RatingUi = RatingUi()
+)
--- app/src/main/java/infinuma/android/shows/details/viewModel/ShowDetailsViewModel.kt
+++ app/src/main/java/infinuma/android/shows/details/viewModel/ShowDetailsViewModel.kt
@@ -1,6 +1,48 @@
 package infinuma.android.shows.details.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import infinuma.android.shows.details.domain.EmailParser
+import infinuma.android.shows.details.models.RatingUi
+import infinuma.android.shows.details.models.ReviewUi
+import infinuma.android.shows.details.models.ShowDetailsUi
+import infinuma.android.shows.login.domain.UserRepository
+import infinuma.android.shows.shows.data.ShowsRepository
 
-class ShowDetailsViewModel: ViewModel() {
+class ShowDetailsViewModel : ViewModel() {
+    private lateinit var userRepository: UserRepository
+    private lateinit var showsRepository: ShowsRepository
+
+    private val _state = MutableLiveData(ShowDetailsUi())
+    val state: LiveData<ShowDetailsUi> = _state
+
+    fun init(
+        showId: String,
+        showsRepository: ShowsRepository,
+        userRepository: UserRepository
+    ) {
+        this.userRepository = userRepository
+        this.showsRepository = showsRepository
+        _state.value = ShowDetailsUi(
+            title = showsRepository.getShow(showId)?.name,
+            username = EmailParser.parseToUsername(userRepository.getUsername() ?: ""),
+            ratingUi = showsRepository.getReviews()
+        )
+    }
+
+    fun addReview(grade: Int, review: String) {
+        _state.value = ShowDetailsUi(
+            _state.value?.title,
+            _state.value?.username,
+            state.value?.ratingUi?.addReview(createReviewUi(grade, review)) ?: RatingUi()
+        )
+    }
+
+    private fun createReviewUi(grade: Int, review: String) = ReviewUi(
+        userRepository.getUserAvatar(),
+        _state.value?.username ?: "",
+        grade,
+        review,
+    )
 }

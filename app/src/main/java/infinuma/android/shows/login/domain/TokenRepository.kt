61741633b73759a87e7cbcdd7505257a2d6f334f
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-26 20:36:13 +0200

    Add token repository

---
+++ app/src/main/java/infinuma/android/shows/login/domain/TokenRepository.kt
@@ -0,0 +1,26 @@
+package infinuma.android.shows.login.domain
+
+import android.content.SharedPreferences
+
+class TokenRepository(
+    private val sharedPreferences: SharedPreferences,
+) {
+
+    companion object {
+        private const val SHARED_PREFS_USER_TOKEN = "sharedPrefsUserToken"
+    }
+
+    private var token = sharedPreferences.getString(SHARED_PREFS_USER_TOKEN, "")
+
+    fun setUserToken(token: String) {
+        if (token != this.token) {
+            with(sharedPreferences.edit()) {
+                putString(SHARED_PREFS_USER_TOKEN, token)
+                apply()
+            }
+            this.token = token
+        }
+    }
+
+    fun getToken() = token
+}

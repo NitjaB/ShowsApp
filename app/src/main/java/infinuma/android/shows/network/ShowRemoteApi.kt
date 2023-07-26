e6acae047f1eb54e22d9f3d8c6e18c57bf169bec
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-26 20:53:17 +0200

    Add login to remote api

--- app/src/main/java/infinuma/android/shows/network/ShowRemoteApi.kt
+++ app/src/main/java/infinuma/android/shows/network/ShowRemoteApi.kt
@@ -1,6 +1,7 @@
 package infinuma.android.shows.network
 
 import infinuma.android.shows.network.models.RegisterUserResponse
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -13,5 +14,12 @@ interface ShowRemoteApi {
         @Field("email") email: String,
         @Field("password") password: String,
         @Field("password_confirmation") passwordConfirmation: String,
+    ): RegisterUserResponse
+
+    @FormUrlEncoded
+    @POST("users/sign_in")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String,
     ): RegisterUserResponse
 }

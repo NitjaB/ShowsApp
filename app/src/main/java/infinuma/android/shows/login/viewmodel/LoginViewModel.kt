6781beb2c106212bb31c1f3af605649ec1d096b6
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-26 20:58:05 +0200

    Handle login button click in login viewModel

--- app/src/main/java/infinuma/android/shows/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/infinuma/android/shows/login/viewmodel/LoginViewModel.kt
@@ -3,11 +3,14 @@ package infinuma.android.shows.login.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import infinuma.android.shows.login.domain.LoginInputError
 import infinuma.android.shows.login.domain.LoginInputValidator
 import infinuma.android.shows.login.domain.UserRepository
 import infinuma.android.shows.login.models.LoginUi
 import infinuma.android.shows.utils.SingleLiveEvent
+import java.lang.Exception
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val _state = MutableLiveData(LoginUi())
@@ -54,7 +57,17 @@ class LoginViewModel : ViewModel() {
     }
 
     fun onLoginButtonClick() {
-        _navigateToShowScreen.value = true
+        viewModelScope.launch {
+            try {
+                userRepository.login(
+                    email = state.value?.email ?: "",
+                    password = state.value?.password ?: ""
+                )
+                _navigateToShowScreen.value = true
+            } catch (e: Exception) {
+
+            }
+        }
     }
 
     private fun validate(

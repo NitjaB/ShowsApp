4c224cb993b0e85411a8d2218a8b55a3ebbbf8c2
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-26 20:56:43 +0200

    Bind tokenRepository and remote api when creating activity

--- app/src/main/java/infinuma/android/shows/main/MainActivity.kt
+++ app/src/main/java/infinuma/android/shows/main/MainActivity.kt
@@ -3,7 +3,9 @@ package infinuma.android.shows.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import infinuma.android.shows.databinding.MainActivityLayoutBinding
+import infinuma.android.shows.network.RemoteApiSingleton
 import infinuma.android.shows.utils.SharedPrefsSource
+import infinuma.android.shows.utils.TokenRepositoryInstance
 import infinuma.android.shows.utils.makeStatusBarTransparent
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityLayoutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        RemoteApiSingleton.bind(applicationContext)
         SharedPrefsSource.init(applicationContext)
+        TokenRepositoryInstance.init(SharedPrefsSource.getSharedPrefs())
         makeStatusBarTransparent()
         binding = MainActivityLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)

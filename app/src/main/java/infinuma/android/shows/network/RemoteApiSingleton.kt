4d037f223f0a459babc972b9e9c73cc390393cea
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-26 20:50:45 +0200

    Implement token extraction and inserion

--- app/src/main/java/infinuma/android/shows/network/RemoteApiSingleton.kt
+++ app/src/main/java/infinuma/android/shows/network/RemoteApiSingleton.kt
@@ -3,20 +3,21 @@ package infinuma.android.shows.network
 import android.content.Context
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import infinuma.android.shows.utils.TokenRepositoryInstance
 import java.util.concurrent.TimeUnit
-import java.util.logging.Level
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-import kotlinx.serialization.modules.EmptySerializersModule
 import okhttp3.Cache
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object RemoteApiSingleton {
 
     private const val BASE_URL = """https://tv-shows.infinum.academy/#section/Tv-Show-API/"""
+    private const val USER_TOKEN_HEADER = "access-token"
 
     private lateinit var okHttp: OkHttpClient
     private lateinit var retrofit: Retrofit
@@ -34,6 +35,7 @@ object RemoteApiSingleton {
         OkHttpClient.Builder()
             .cache(Cache(appContext.cacheDir, 1024 * 1024 * 10L))
             .connectTimeout(5, TimeUnit.MINUTES)
+            .addNetworkInterceptor(createTokenInterceptor())
             .addNetworkInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             })
@@ -56,5 +58,22 @@ object RemoteApiSingleton {
 
     private fun createShowRemoteApi() = retrofit.create(ShowRemoteApi::class.java)
 
+    private fun createTokenInterceptor(): (chain: Interceptor.Chain) -> Response = { chain ->
+        val originalRequest = chain.request()
+        val token = TokenRepositoryInstance.get().getToken()
+        val newRequest = if (!token.isNullOrBlank()) {
+            originalRequest.newBuilder()
+                .header(USER_TOKEN_HEADER, token)
+                .method(originalRequest.method, originalRequest.body)
+                .build()
+        } else {
+            originalRequest
+        }
 
+        val response = chain.proceed(newRequest)
+        TokenRepositoryInstance.get().setUserToken(
+            response.headers[USER_TOKEN_HEADER] ?: ""
+        )
+        response
+    }
 }

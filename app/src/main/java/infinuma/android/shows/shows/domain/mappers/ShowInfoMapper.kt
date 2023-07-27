8b102aadfffd6d6782eb8d9d10876a269d49b202
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-27 23:13:06 +0200

    Add logic to map DisplayShowResponse to ShowInfo

--- app/src/main/java/infinuma/android/shows/shows/domain/mappers/ShowInfoMapper.kt
+++ app/src/main/java/infinuma/android/shows/shows/domain/mappers/ShowInfoMapper.kt
@@ -1,24 +1,27 @@
 package infinuma.android.shows.shows.domain.mappers
 
+import infinuma.android.shows.network.models.DisplayShowResponse
 import infinuma.android.shows.network.models.ListShowsResponse
+import infinuma.android.shows.network.models.ShowResponse
 import infinuma.android.shows.shows.domain.models.ShowInfo
 
 class ShowInfoMapper {
 
+    fun fromResponse(response: DisplayShowResponse) = mapShow(response.show)
+
     fun fromResponse(response: ListShowsResponse): List<ShowInfo> {
         val showsInfo = mutableListOf<ShowInfo>()
-        response.shows.forEach {
-            showsInfo.add(
-                ShowInfo(
-                    id = it.id,
-                    averageRating = it.average_rating ?: 0f,
-                    description = it.description ?: "",
-                    imageUrl = it.imageUrl,
-                    numberOfReviews = it.numberOfReviews,
-                    title = it.title
-                )
-            )
-        }
+        response.shows.forEach { showsInfo.add(mapShow(it)) }
         return showsInfo
     }
+
+    private fun mapShow(show: ShowResponse) =
+        ShowInfo(
+            id = show.id,
+            averageRating = show.average_rating ?: 0f,
+            description = show.description ?: "",
+            imageUrl = show.imageUrl,
+            numberOfReviews = show.numberOfReviews,
+            title = show.title
+        )
 }

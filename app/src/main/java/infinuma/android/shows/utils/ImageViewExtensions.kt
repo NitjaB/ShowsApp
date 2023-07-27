262f5befe70e316d74a887a9741e2e6337b6a4a9
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-27 21:58:55 +0200

    Add extension function for loading images with url

--- app/src/main/java/infinuma/android/shows/utils/ImageViewExtensions.kt
+++ app/src/main/java/infinuma/android/shows/utils/ImageViewExtensions.kt
@@ -3,9 +3,17 @@ package infinuma.android.shows.utils
 import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import infinuma.android.shows.R
 
 fun ImageView.loadWithGlide(bitmap: Bitmap?) {
     Glide.with(context)
         .load(bitmap)
+        .into(this)
+}
+
+fun ImageView.loadWithGlide(imageUrl: String) {
+    Glide.with(context)
+        .load(imageUrl)
+        .error(R.drawable.ic_image_loading_error)
         .into(this)
 }

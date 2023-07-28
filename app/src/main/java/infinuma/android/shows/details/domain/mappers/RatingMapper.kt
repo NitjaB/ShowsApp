afc31229145b3c5a1c3f9766d24bf02f3b015eeb
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-28 18:22:23 +0200

    Add RatingMapper

---
+++ app/src/main/java/infinuma/android/shows/details/domain/mappers/RatingMapper.kt
@@ -0,0 +1,30 @@
+package infinuma.android.shows.details.domain.mappers
+
+import infinuma.android.shows.details.domain.models.Rating
+import infinuma.android.shows.details.domain.models.Review
+import infinuma.android.shows.network.models.ListReviewsResponse
+
+class RatingMapper {
+
+    fun mapFromResource(
+        numberOfReviews: Int,
+        averageGrade: Float,
+        listReviewsResponse: ListReviewsResponse
+    ) {
+        Rating(
+            numberOfReviews = numberOfReviews,
+            averageReviewGrade = averageGrade,
+            reviews = mapFromResource(listReviewsResponse)
+        )
+    }
+
+    private fun mapFromResource(listReviewsResponse: ListReviewsResponse) =
+        listReviewsResponse.reviewsResponse.map { response ->
+            Review(
+                avatarUrl = response.user.avatarUrl ?: "",
+                review = response.comment,
+                starGrade = response.rating,
+                username = response.user.email
+            )
+        }
+}

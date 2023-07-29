617bb2b144e2355822d916653bac52691a8c0037
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-29 19:57:17 +0200

    Add room instance

---
+++ app/src/main/java/infinuma/android/shows/database/RoomInstance.kt
@@ -0,0 +1,50 @@
+package infinuma.android.shows.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import infinuma.android.shows.database.dao.ReviewDao
+import infinuma.android.shows.database.dao.ShowDao
+
+object RoomInstance {
+
+    private lateinit var database: ShowDatabase
+
+    fun instantiate(context: Context) {
+        database = ShowDatabase.getDatabase(context)
+    }
+
+    fun get() = database
+
+}
+
+@Database(entities = [ReviewDao::class, ShowDao::class], version = 1)
+abstract class ShowDatabase : RoomDatabase() {
+
+    abstract fun reviewDao(): ReviewDao
+
+    abstract fun showDao(): ShowDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: ShowDatabase? = null
+
+        fun getDatabase(context: Context): ShowDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ShowDatabase::class.java,
+                    "show_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}

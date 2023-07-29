47ce848b6b5c09d21dc78d3b9d6193f05ef7474f
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-07-29 19:43:18 +0200

    Make review domain model entity

--- app/src/main/java/infinuma/android/shows/details/domain/models/Review.kt
+++ app/src/main/java/infinuma/android/shows/details/domain/models/Review.kt
@@ -1,6 +1,11 @@
 package infinuma.android.shows.details.domain.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity("review_table")
 data class Review(
+    @PrimaryKey
     val id: String,
     val avatarUrl: String,
     val username: String,

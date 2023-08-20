c49b5b70a98eba6f1462463c69d5bf219c90a59a
Infinum <infinum@Infinums-MacBook-Pro.local>
2023-08-20 20:32:04 +0200

    Add updateShow function in ShowDao

--- app/src/main/java/infinuma/android/shows/database/dao/ShowDao.kt
+++ app/src/main/java/infinuma/android/shows/database/dao/ShowDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import infinuma.android.shows.shows.domain.models.ShowInfo
 
 @Dao
@@ -11,9 +12,13 @@ interface ShowDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(showInfo: ShowInfo)
+
     @Query("SELECT * FROM show_table")
     fun getAllShows(): List<ShowInfo>
 
     @Query("SELECT * FROM show_table where id = :id")
     fun getShowById(id: String): ShowInfo
+
+    @Update(entity = ShowInfo::class)
+    fun updateShow(showInfo: ShowInfo)
 }
